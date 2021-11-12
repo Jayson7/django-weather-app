0b5eacf9e15cd4d83d9dd3d61f2365e885666238
jayson7 <lexxiijoo70@gmail.com>
2021-11-12 11:47:05 +0100

    weather app

--- weather_forcast/settings.py
+++ weather_forcast/settings.py
@@ -117,12 +117,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+import os
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
    
 ]
+MEDIA_URL ="/images/"
+MEDIA_ROOT = os.path.join(BASE_DIR, '/images/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- weather_forcast/urls.py
+++ weather_forcast/urls.py
@@ -2,9 +2,13 @@ from apps.views import *
 from apps import views
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.homepage, name="home"),
-
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
