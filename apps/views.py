bb244e9bfa0de8f9072eda3abcdd46f1b3d1f033
jayson7 <lexxiijoo70@gmail.com>
2021-11-12 08:27:05 +0100

    weather app

--- apps/views.py
+++ apps/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+import json
+import urllib
 # Create your views here.
+
+def homepage(request):
+    if request.method == 'POST':
+        city = request.POST['city']
+        url = urllib.request.urlopen('api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&appid=979c31c159d536fafdc155c66bd467eb').read()
+        print(url)
+        
+    return render(request, 'index.html')
