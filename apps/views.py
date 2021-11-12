a2ad3f6f2de3233a9d7fd3b92c9381ba00a373a9
jayson7 <lexxiijoo70@gmail.com>
2021-11-12 08:52:17 +0100

    weather app

--- apps/views.py
+++ apps/views.py
@@ -6,7 +6,8 @@ import urllib
 def homepage(request):
     if request.method == 'POST':
         city = request.POST['city']
-        url = urllib.request.urlopen('api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&appid=979c31c159d536fafdc155c66bd467eb').read()
+        url = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&appid=979c31c159d536fafdc155c66bd467eb').read()
+        list_data = json.loads(url)
         print(url)
         
     return render(request, 'index.html')
